a38a641e9ad19dc99051113bd9dc479d7e80240c
Seamus O'Connor <shinra.electric@icloud.com>
2025-08-19 15:26:56 +0900

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,5 @@ Rails.application.routes.draw do
 
   get "up" => "rails/health#show", as: :rails_health_check
 
-  resources :chats do
-    resources :routines
-  end
-  
+  resources :routines, only: [:index, :show, :create, :new, :edit, :update, :destroy]
 end
