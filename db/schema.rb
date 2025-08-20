f7f957c89cbda34a22e230b4ddccebe389838066
Seamus O'Connor <shinra.electric@icloud.com>
2025-08-20 11:41:59 +0900

    finish exercise seeding

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_08_19_052128) do
+ActiveRecord::Schema[7.1].define(version: 2025_08_20_023504) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,8 @@ ActiveRecord::Schema[7.1].define(version: 2025_08_19_052128) do
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "muscle_group"
+    t.string "img_url"
   end
 
   create_table "messages", force: :cascade do |t|
