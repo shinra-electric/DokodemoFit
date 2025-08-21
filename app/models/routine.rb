d44528247a3f497b29ba2af6464d49736d524240
Youssef Sarraj <y.sarraj@outlook.com>
2025-08-21 16:08:45 +0900

    added tool_call model

--- app/models/routine.rb
+++ app/models/routine.rb
@@ -1,10 +1,9 @@
 class Routine < ApplicationRecord
+  acts_as_chat
   belongs_to :user
   has_many :messages, dependent: :destroy
   has_many :exercises, through: :routine_exercises
   has_many :routine_exercises, dependent: :destroy
-
   validates :title, presence: true
   validates :goal, presence: true
-
 end
