d3fdf68cb86324ff3acd7eab49b4136dd1cb73f6
Seamus O'Connor <shinra.electric@icloud.com>
2025-08-22 12:34:50 +0900

    Add TODO

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
   def home
+    # TODO: remove after the presentation
     if user_signed_in?        
       redirect_to routines_path    
     end
